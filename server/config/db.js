bd4af77ed4674526dda216ead3702d1d2db5564f
kunal23pa <kunalpatel5813@gmail.com>
2023-08-12 05:37:18 -0400

    Final Release

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://jashan:harmanbains@1@cluster0.lhvspfx.mongodb.net/?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://kunalpatel5813:Password@shelternet.bdixnuw.mongodb.net/?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
